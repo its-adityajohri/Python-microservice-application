1fabf3a9b0b26e0fec609ff2691dc0182eded013
Aditya Johri <aj.adityajohri@gmail.com>
2024-02-02 23:58:12 +0530

    Initial basic client update

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Products from "./admin/Products";
+import {BrowserRouter, Route} from "react-router-dom";
+import Main from "./main/Main";
+import ProductsCreate from "./admin/ProductsCreate";
+import ProductsEdit from "./admin/ProductsEdit";
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Route path='/' Component={Main}/>
+                <Route path='/admin/products' Component={Products}/>
+                <Route path='/admin/products/create' Component={ProductsCreate}/>
+                <Route path='/admin/products/:id/edit' Component={ProductsEdit}/>
+            </BrowserRouter>
+
+        </div>
+    );
+}
+
+export default App;
